2a43facf7421c0f76dc390c3125f23c377fc0d4b
TheBestCupcake <cavaligrooby@gmail.com>
2025-12-18 13:40:52 +1300

    Added a date calculator to calculate time between depature.

---
+++ utils/date-calculator.js
@@ -0,0 +1,11 @@
+
+const dateOfDepature = new Date("2025-11-13");
+const dateOfReturn = "Add Later";
+
+const currentDate = new Date();
+
+function daysSinceDeparture(currentDate, dateOfDepature){
+    let timeDifference = currentDate - dateOfDepature;
+    let daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    return daysDifference;
+}
